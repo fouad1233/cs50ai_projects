c1bc55848489a3f759b4360d0c31b2be9db332e1
Fouad Aladhami <fouadiadhami@gmail.com>
2024-06-22 13:14:05 +0300

    puzzle.py completed succesfully
    it is checked with check 50

--- lecture1/knights/puzzle.py
+++ lecture1/knights/puzzle.py
@@ -12,21 +12,47 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    #can't be both knight and knave bu it can be one of them
+    Not(And(AKnight, AKnave)),
+    Or(AKnight, AKnave),
+    
+    
+    Implication( AKnight , And(AKnight, AKnave)),
+    Implication(AKnave, Not(And(AKnight, AKnave))),
+    
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # A can't be both knight and knave but it can be one of them
+    Not(And(AKnight, AKnave)),
+    Or(AKnight, AKnave),
+    # B can't be both knight and knave but it can be one of them
+    Not(And(BKnight, BKnave)),
+    Or(BKnight, BKnave),
+    
+    Implication(AKnight, And(AKnave, BKnave)),  
+    Implication(AKnave, Not(And(AKnave, BKnave))),
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # A can't be both knight and knave but it can be one of them
+    Not(And(AKnight, AKnave)),
+    Or(AKnight, AKnave),
+    # B can't be both knight and knave but it can be one of them
+    Not(And(BKnight, BKnave)),
+    Or(BKnight, BKnave),
+    
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight)))),
 )
 
 # Puzzle 3
@@ -35,7 +61,32 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    # A can't be both knight and knave but it can be one of them
+    Not(And(AKnight, AKnave)),
+    Or(AKnight, AKnave),
+    # B can't be both knight and knave but it can be one of them
+    Not(And(BKnight, BKnave)),
+    Or(BKnight, BKnave),
+    # C can't be both knight and knave but it can be one of them
+    Not(And(CKnight, CKnave)),
+    Or(CKnight, CKnave),
+    
+    Implication(BKnight ,And(
+                            Implication(AKnight, AKnave),
+                            Implication(AKnave, Not(AKnave))
+                            )
+                ),
+    Implication(BKnight ,Not(And(
+                            Implication(AKnight, AKnave),
+                            Implication(AKnave, Not(AKnave))
+                            ))
+                ),
+    
+    Implication(BKnight, CKnave),
+    Implication(BKnave, Not(CKnave)),
+    
+    Implication(CKnight, AKnight),
+    Implication(CKnave, Not(AKnight)),
 )
 
 
