13a8dadf27a6a85639430503c9963830e33524c2
Fouad Aladhami <fouadiadhami@gmail.com>
2024-08-12 12:26:09 +0300

    Update (preprocess function implemented)

--- lecture6/parser/parser.py
+++ lecture6/parser/parser.py
@@ -62,7 +62,15 @@ def preprocess(sentence):
     and removing any word that does not contain at least one alphabetic
     character.
     """
-    raise NotImplementedError
+    tokenized_words = nltk.tokenize.word_tokenize(sentence)
+    for i in range(len(tokenized_words)):
+        tokenized_words[i] = tokenized_words[i].lower()
+    words = []
+    for word in tokenized_words:
+        if any(charachter.isalpha() for charachter in word):
+            words.append(word)
+    return words
+
 
 
 def np_chunk(tree):
